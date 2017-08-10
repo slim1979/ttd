8cd0b69d48afae54adfafa41f90138c57b60c56d
slim1979 <chinezan@yandex.ru>
2017-08-10 22:42:14 +0300

    task 2 done

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,4 +2,5 @@ class Route < ApplicationRecord
   validates :title, presence: true
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
+  has_many :trains
 end

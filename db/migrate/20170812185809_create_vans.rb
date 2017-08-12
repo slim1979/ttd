c3b0dc98be1c4c9f21e4ead4d46548ab16550c66
slim1979 <chinezan@yandex.ru>
2017-08-12 23:20:26 +0300

    vans model, migration, controller

--- db/migrate/20170812185809_create_vans.rb
+++ db/migrate/20170812185809_create_vans.rb
@@ -1,8 +1,10 @@
 class CreateVans < ActiveRecord::Migration[5.1]
   def change
     create_table :vans do |t|
-      t.string     :kind
-      t.belongs_to :train
+      t.string      :kind
+      t.integer     :up_seats
+      t.integer     :down_seats
+      t.belongs_to  :train
       t.timestamps
     end
   end

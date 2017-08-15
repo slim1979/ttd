4959ff2edee5c4023663211bf1bf4543fb6141a7
slim1979 <chinezan@yandex.ru>
2017-08-15 22:04:12 +0300

    stations count, set title

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,12 +4,19 @@ class Route < ApplicationRecord
   has_many :trains
 
   validates :title, presence: true
+  validate :stations_count
 
   before_validation :set_title
 
   private
 
   def set_title
-    self.title ||= "#{railway_stations.first.title} - #{railway_stations.last.title}"
+    self.title = "#{railway_stations.first.title} - #{railway_stations.last.title}"
+  rescue NoMethodError
+    errors.add(:base, 'Нельзя создать маршрут без станций')
+  end
+
+  def stations_count
+    errors.add(:base, 'Маршрут должен включать не менее 2-х станций') if railway_stations.size < 2
   end
 end

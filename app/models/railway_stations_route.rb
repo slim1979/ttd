84865f2df3ae7cc39f49d1e9ddaea3b8cb53d700
slim1979 <chinezan@yandex.ru>
2017-08-16 23:46:51 +0300

    increase and decrease scopes, validation uniqueness

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -1,4 +1,9 @@
 class RailwayStationsRoute < ApplicationRecord
   belongs_to :railway_station
   belongs_to :route
+
+  validates :railway_station_index, uniqueness: { scope: :route }
+
+  scope :increase, -> { order(:railway_station_index) }
+  scope :decrease, -> { order(railway_station_index: :desc) }
 end

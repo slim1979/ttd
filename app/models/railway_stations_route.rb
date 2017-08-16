3ca6ef2d498eb7d6fdcb893792f70c74a4eee2f5
slim1979 <chinezan@yandex.ru>
2017-08-17 01:18:59 +0300

    set index before validation

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -3,7 +3,14 @@ class RailwayStationsRoute < ApplicationRecord
   belongs_to :route
 
   validates :railway_station_index, uniqueness: { scope: :route }
+  before_validation :set_index
 
   scope :increase, -> { order(:railway_station_index) }
   scope :decrease, -> { order(railway_station_index: :desc) }
+
+  private
+
+  def set_index
+    self.railway_station_index ||= self.railway_station_id
+  end
 end

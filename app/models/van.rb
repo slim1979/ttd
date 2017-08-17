9f5bb1fb06805f25346ff0c0b93e01465259fc17
slim1979 <chinezan@yandex.ru>
2017-08-17 11:38:09 +0300

    show seats

--- app/models/van.rb
+++ app/models/van.rb
@@ -2,7 +2,11 @@ class Van < ApplicationRecord
   belongs_to :train
   belongs_to :van_kind
 
-  validates :van_kind, presence: true
-  validates :up_seats, presence: true
-  validates :down_seats, presence: true
+  def show_seats
+    van_seats = { 'SedentaryVans' => %i[seats],
+                  'PlatzVan'      => %i[top_seats bottom_seats side_top_seats side_bottom_seats],
+                  'CoupeVan'      => %i[top_seats bottom_seats],
+                  'SleepingVan'   => %i[bottom_seats] }
+    van_seats[type]
+  end
 end

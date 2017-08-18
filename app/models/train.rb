f8f0fd5be86efb142012d57a6917d7477b3d1f86
slim1979 <chinezan@yandex.ru>
2017-08-18 18:13:15 +0300

    form and methods to take places amount through van type

--- app/models/train.rb
+++ app/models/train.rb
@@ -5,6 +5,7 @@ class Train < ApplicationRecord
   has_many :vans
 
   validates :number, presence: true
+  before_save :set_sort_by
 
   def platz_vans
     vans.where(van_kind: 1)
@@ -20,10 +21,27 @@ class Train < ApplicationRecord
     all
   end
 
+  def set_sort_by
+    self.sort_by = 'asc' if self.new_record?
+  end
+
   def sort_vans
     vans.order(number: sort_by.to_sym)
   end
 
+  def van_has_such_seats?(van, seats)
+    vans.where(type: van).map(&seats).include? nil
+  end
+
   def adv_info
+    if places_type_to_more_info && van_type_to_more_info
+      seats = places_type_to_more_info.to_sym
+      van = van_type_to_more_info.to_sym
+      if van_has_such_seats?(van, seats)
+        'В вагоне этого типа нет таких мест'
+      else
+        vans.where(type: van).map(&seats).sum unless van_has_such_seats?(van, seats)
+      end
+    end
   end
 end

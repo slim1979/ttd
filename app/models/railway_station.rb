3a1ed2e216dbd2526476110d0a34817bab9a1381
slim1979 <chinezan@yandex.ru>
2017-08-18 21:51:41 +0300

    refactoring, remove duplication

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -5,15 +5,19 @@ class RailwayStation < ApplicationRecord
 
   validates :title, presence: true
 
+  def station(route)
+    RailwayStationsRoute.where(railway_station: self, route: route).first
+  end
+
   def set_index_on(route, index)
-    station = RailwayStationsRoute.where(railway_station: self, route: route).first
+    station = station(route)
     station.railway_station_index = index
     station.save if index.is_a? Integer
     raise ArgumentError, 'Индекс не может быть строкой' unless index.is_a? Integer
   end
 
   def show_index_on(route)
-    station = RailwayStationsRoute.where(railway_station: self, route: route).first
+    station = station(route)
     station.railway_station_index
   end
 end

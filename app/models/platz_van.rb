8eee29c9e4e4df23c7ba4eee84b9f892e9f05edc
slim1979 <chinezan@yandex.ru>
2017-08-18 18:19:26 +0300

    validation seats precense, befor validation callback

--- app/models/platz_van.rb
+++ app/models/platz_van.rb
@@ -1,4 +1,3 @@
 class PlatzVan < Van
-  # validates :top_seats, :bottom_seats, :side_top_seats, :side_bottom_seats, presence: true
-  # validates :seats, presence: nil
+  validates :top_seats, :bottom_seats, :side_top_seats, :side_bottom_seats, presence: true
 end

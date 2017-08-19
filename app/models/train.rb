912c62a27d23ad74d25a4e674575ce2c2af0e854
slim1979 <chinezan@yandex.ru>
2017-08-19 23:29:40 +0300

    bug fixed

--- app/models/train.rb
+++ app/models/train.rb
@@ -29,15 +29,11 @@ class Train < ApplicationRecord
     vans.order(number: sort_by.to_sym)
   end
 
-  def van_has_such_seats?(van, seats)
-    !vans.where(type: van).map(&seats).include? nil
-  end
-
   # vans collection with uniq values to collection select
   # in show template
   def vans_collection
     collection = []
-    vans.map(&:type).uniq.each { |type| collection << Van::TYPE[type.to_sym] }
+    vans.map(&:type).uniq.each { |type| collection << [type.to_sym, Van::TYPE[type.to_sym]] }
     collection
   end
 
@@ -45,7 +41,9 @@ class Train < ApplicationRecord
   def adv_info
     seats = places_type_to_more_info.to_sym if places_type_to_more_info
     van = van_type_to_more_info.to_sym if van_type_to_more_info
-    vans.where(type: van).map(&seats).sum if van_has_such_seats?(van, seats)
-    'В вагоне этого типа нет таких мест' unless van_has_such_seats?(van, seats)
+    van_seats = vans.where(type: van).map(&seats)
+    type_mismatch = van_seats.include? nil
+    'В вагоне этого типа нет таких мест' if type_mismatch
+    van_seats.sum unless type_mismatch
   end
 end
